dbbcc3348f97d1b14edaa861b9988be1a7ea2505
Aabhas Jena <aabhasjena2003@gmail.com>
2026-02-25 17:33:25 +0530

    update loan product

--- backend/schemas/admin/loanProducts/update.schema.js
+++ backend/schemas/admin/loanProducts/update.schema.js
@@ -1,6 +1,7 @@
 const { z } = require("zod");
 
 exports.updateLoanProductSchema = z.object({
+  code: z.string().optional(), // enum string
   name: z.string().min(2).optional(),
   description: z.string().optional(),
 });
