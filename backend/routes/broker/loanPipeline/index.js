34c9b77e37da49f449ccba671c1ad3edc6475577
Aabhas Jena <aabhasjena2003@gmail.com>
2026-03-12 12:07:12 +0530

    loi to broker dashboard

--- backend/routes/broker/loanPipeline/index.js
+++ backend/routes/broker/loanPipeline/index.js
@@ -1,6 +1,9 @@
 async function loanPipelineRoutes(fastify) {
   fastify.register(require("./submissionDocuments"), { prefix: "" });
   fastify.register(require("./uploadSubmissionDocument"),{prefix:""});
+  fastify.register(require("./listLoi"));   // NEW
+  fastify.register(require("./viewLoi"));
+  fastify.register(require("./listLender"));
 }
 
 module.exports = loanPipelineRoutes;
