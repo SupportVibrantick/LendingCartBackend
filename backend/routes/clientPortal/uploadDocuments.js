63a84216fa09df79dd94bf428504438b98671206
Aabhas Jena <aabhasjena2003@gmail.com>
2026-03-17 12:20:03 +0530

    path bug

--- backend/routes/clientPortal/uploadDocuments.js
+++ backend/routes/clientPortal/uploadDocuments.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 const { loadTemplate } = require("../../utils/loadTemplate");
-const sendMail = require("../../../services/mail");
-const { sendEmailUsingKafka } = require("../../../services/kafka/email/producer");
+const sendMail = require("../../services/mail");
+const { sendEmailUsingKafka } = require("../../services/kafka/email/producer");
 
 /**
  * @param {import("fastify").FastifyInstance} fastify
