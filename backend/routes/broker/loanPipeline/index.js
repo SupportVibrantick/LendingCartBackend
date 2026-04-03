669ca617089ff341ee61125b3399f7fdf259735d
Aabhas Jena <aabhasjena2003@gmail.com>
2026-04-03 13:37:47 +0530

    delete doc api

--- backend/routes/broker/loanPipeline/index.js
+++ backend/routes/broker/loanPipeline/index.js
@@ -1,7 +1,7 @@
 async function loanPipelineRoutes(fastify) {
   fastify.register(require("./submissionDocuments"), { prefix: "" });
   fastify.register(require("./uploadSubmissionDocument"),{prefix:""});
-  fastify.register(require("./listLoi"));   // NEW
+  fastify.register(require("./listLoi"));   
   fastify.register(require("./viewLoi"));
   fastify.register(require("./listLender"));
 
@@ -9,7 +9,8 @@ async function loanPipelineRoutes(fastify) {
   fastify.register(require("./sendClientLink"));
   fastify.register(require("./requestDocuments"));
   fastify.register(require("./submitDocumentsToLender"));
-  
+
+  fastify.register(require("./deleteDocument.js"));
 }
 
 module.exports = loanPipelineRoutes;
