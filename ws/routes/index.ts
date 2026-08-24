571a7ae48ac34480216b23812f065b79568fda1f
Akash Patel <akash906129@gmail.com>
2026-08-24 10:41:55 +0530

    init new ws

--- ws/routes/index.ts
+++ ws/routes/index.ts
@@ -2,9 +2,11 @@ import type { Socket } from "socket.io";
 
 import { healthRoutes } from "./health";
 import { adminRoutes } from "./broker_admin/broker";
+import { notificationRoutes } from "./broker_admin/notifications";
 
 
 export function registerSocketRoutes(socket: Socket) {
     healthRoutes(socket);
     adminRoutes(socket);
+    notificationRoutes(socket);
 }
