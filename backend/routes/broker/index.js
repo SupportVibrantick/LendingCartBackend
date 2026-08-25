7e2ec5d7f6777bb16e4cf93f791f9798079d4e74
Akash Patel <akash906129@gmail.com>
2026-08-25 12:35:15 +0530

    removed dead swegger

--- backend/routes/broker/index.js
+++ backend/routes/broker/index.js
@@ -35,16 +35,6 @@ module.exports = async function brokerRoutes(fastify, opts) {
     instance.register(require("../../plugins/verifyBrokerSubscription"));
 
     instance.addHook("preHandler", async (req, reply) => {
-      // Allow Swagger
-      if (
-        req.url.startsWith("/docs") ||
-        req.url.startsWith("/swagger") ||
-        req.url.includes("/docs") ||
-        req.url.includes("swagger")
-      ) {
-        return;
-      }
-
       // JWT auth
       await instance.authenticate(req, reply);
 
