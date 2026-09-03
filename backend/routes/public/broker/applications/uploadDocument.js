753ecf0cfc4dcc2e3941f04aefc2e833f5fd23dd
akash patel <akash906129@gmail.com>
2026-09-03 14:07:41 +0530

    fixed file import issue

--- backend/routes/public/broker/applications/uploadDocument.js
+++ backend/routes/public/broker/applications/uploadDocument.js
@@ -14,7 +14,7 @@ const fs = require("fs");
 const path = require("path");
 const { pipeline } = require("stream/promises");
 const crypto = require("crypto");
-const { validateFileMimetype } = require("../../../utils/security/fileValidator");
+const { validateFileMimetype } = require("../../../../utils/security/fileValidator");
 
 async function uploadDocumentRoute(fastify) {
   fastify.post(
